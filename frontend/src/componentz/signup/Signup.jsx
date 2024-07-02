1b827f9012fd79aff4b02f978f39c47da7a313c3
RitikBishwas <134728719+RitikBishwas@users.noreply.github.com>
2024-07-02 08:29:06 +0530

    signup fixed

--- frontend/src/componentz/signup/Signup.jsx
+++ frontend/src/componentz/signup/Signup.jsx
@@ -1,82 +1,75 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React from "react";
 import "./signup.css";
 import HeadingComp from "./HeadingComp";
-
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
-  const navigate = useNavigate();
-  const [inputs, setInputs] = useState({
+  const history = useNavigate();
+  const [Inputs, setInputs] = useState({
     email: "",
     username: "",
     password: "",
   });
-
-  const handleChange = (e) => {
+  const change = (e) => {
     const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
+    setInputs({ ...Inputs, [name]: value });
   };
-
-  const handleSubmit = async (e) => {
+  const submit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post(
-        `${window.location.origin}/api/v1/register`,
-        inputs
-      );
-      if (response.data.message === "User Already Exists") {
-        alert(response.data.message);
-      } else {
-        alert(response.data.message);
-        setInputs({
-          email: "",
-          username: "",
-          password: "",
-        });
-        navigate("/signin");
-      }
-    } catch (error) {
-      console.error("Error during registration:", error);
-      alert("An error occurred during registration. Please try again later.");
-    }
+    await axios
+      .post(`${window.location.origin}/api/v1/register`, Inputs)
+      .then((response) => {
+        if (response.data.message === "User Already Exists") {
+          alert(response.data.message);
+        } else {
+          alert(response.data.message);
+          setInputs({
+            email: "",
+            username: "",
+            password: "",
+          });
+          history("/signin");
+        }
+      });
   };
-
   return (
     <div className="signup">
       <div className="container">
         <div className="row">
-          <div className="col-lg-8 column d-flex justify-content-center align-items-center">
-            <div className="d-flex flex-column w-100 p-3">
+          <div className="col-lg-8 column d-flex justify-content-center align-items-center ">
+            <div className="d-flex flex-column  w-100 p-3">
               <input
-                className="p-2 my-3 input-signup"
+                className="p-2  my-3 input-signup"
                 type="email"
                 name="email"
                 placeholder="Enter Your Email"
-                onChange={handleChange}
-                value={inputs.email}
+                onChange={change}
+                value={Inputs.email}
               />
               <input
                 className="p-2 my-3 input-signup"
-                type="text"
+                type="username"
                 name="username"
                 placeholder="Enter Your Username"
-                onChange={handleChange}
-                value={inputs.username}
+                onChange={change}
+                value={Inputs.username}
               />
               <input
                 className="p-2 my-3 input-signup"
                 type="password"
                 name="password"
                 placeholder="Enter Your Password"
-                onChange={handleChange}
-                value={inputs.password}
+                onChange={change}
+                value={Inputs.password}
               />
-              <button className="btn-signup p-2" onClick={handleSubmit}>
+
+              <button className="btn-signup p-2" onClick={submit}>
                 Sign Up
               </button>
             </div>
           </div>
-          <div className="col-lg-4 column col-left d-lg-flex justify-content-center align-items-center d-none">
+          <div className=" col-lg-4 column col-left d-lg-flex justify-content-center align-items-center  d-none">
             <HeadingComp first="Sign" second="Up" />
           </div>
         </div>
